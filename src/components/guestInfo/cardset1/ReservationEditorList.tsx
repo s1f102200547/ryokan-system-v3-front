d855d455e2520bb65b06e5d913411ddeac1c380d
s1f102200547 <s1f102200547@iniad.org>
2026-05-07 19:13:57 +0900

    nameをboxテキストフィールドではなくstandard textfirldにする

--- src/components/guestInfo/cardset1/ReservationEditorList.tsx
+++ src/components/guestInfo/cardset1/ReservationEditorList.tsx
@@ -162,7 +162,7 @@ export function ReservationEditorList({ localData, nights, onFieldChange }: Prop
             <TextField
               value={localData.guest_name}
               onChange={(e) => onFieldChange('guest_name', e.target.value)}
-              size="small" fullWidth variant="outlined"
+              size="small" fullWidth variant="standard"
               sx={compactInputSx}
             />
           </Box>
