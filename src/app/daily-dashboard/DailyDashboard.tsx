04b5769de01b270bcd2c3b0dd4fa869db41a4b0b
s1f102200547 <s1f102200547@iniad.org>
2026-05-11 00:17:54 +0900

    露天・夕食・CI時間・朝食トグルをもっと上に表示

--- src/app/daily-dashboard/DailyDashboard.tsx
+++ src/app/daily-dashboard/DailyDashboard.tsx
@@ -181,7 +181,7 @@ export function DailyDashboard({ today }: Props) {
           selectedDate={selectedDate}
           selectedToggle={selectedToggle}
           topContent={
-            <Box sx={{ display: 'flex', justifyContent: 'center', mb: 1 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: -4, mb: 1 }}>
               <ToggleButtonGroup
                 value={selectedToggle}
                 exclusive
