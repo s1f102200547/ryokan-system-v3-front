b53af6f0f9679dc4d6d7da3d12fe76aa9bc8ebd5
s1f102200547 <s1f102200547@iniad.org>
2026-05-10 12:14:24 +0900

    不要なコードを削除

--- src/app/a_tax_table/page.tsx
+++ src/app/a_tax_table/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
 import { DashboardTabs } from '@/components/DashboardTabs'
 import { ATaxTable } from '@/components/aTaxTable/ATaxTable'
 
@@ -10,12 +8,6 @@ export default function ATaxTablePage() {
   return (
     <Container maxWidth="xl" sx={{ py: 3 }}>
       <DashboardTabs />
-
-      <Box sx={{ display: 'flex', alignItems: 'center', mb: 2, gap: 2 }}>
-        <Typography variant="h6" fontWeight={700}>
-          宿泊税管理
-        </Typography>
-      </Box>
       <ATaxTable />
     </Container>
   )
